01d6945b62b4301ff5c7c627c6f19c225c1eebec
Nonso Derrick Vincent <36786664+ihamquentin@users.noreply.github.com>
2022-02-02 19:00:47 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-west-2_Gl1gpDfZt",
+    userPoolClientId: "48dnc3fu1rjsg8lifvpms7cslm",
+    region: "us-west-2"
+  },
+  api: {
+    invokeUrl: ""
+  }
+};
